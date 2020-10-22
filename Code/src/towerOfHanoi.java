a43cc5ab44c6477e68bd0087c22e80b832f87432
Neha-Bedi <18nehabedi@gmail.com>
2020-10-22 17:11:48 +0530

    towerOfHanoi.java

--- Code/src/towerOfHanoi.java
+++ Code/src/towerOfHanoi.java
@@ -13,8 +13,6 @@ public class towerOfHanoi {
 
 
         }
-        import java.util.Scanner;
-
     public class runner {
         public static void main(String[] args) {
             Scanner s = new Scanner(System.in);
